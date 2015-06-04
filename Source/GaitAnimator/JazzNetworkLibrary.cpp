d0577540fb38e28a9cb74dc8e2c4ba0041a14905
Michele Bianchi <jazzinghen@gmail.com>
2015-06-04 19:19:31 +0900

    Removed tentative JazzNetwork Library
    It is time to do some cleanup, people!

--- Source/GaitAnimator/JazzNetworkLibrary.cpp
+++
@@ -1,8 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "GaitAnimator.h"
-#include "JazzNetworkLibrary.h"
-
-bool UJazzNetworkLibrary::NewDataAvailable(){
-	return true;
-}
